d091a98148fe94995225ddaa21b38409cb43e5d9
ChinmaiHK <96460827+ChinmaiHK@users.noreply.github.com>
2022-10-20 10:35:38 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -8,7 +8,11 @@ r=a+b+c
 m=a*b*c
 d=b-a
 d1=c-b
+div=a/b
+div1=b/c
 print('The sum of the three numbers is',r)
 print('The product of the three numbers is',m)
 print('The difference of the first two numbers is',d)
 print('The difference of the next two numbers is',d1)
+print('The division of the first two numbers is',div)
+print('The division of the next two numbers is',div1)
