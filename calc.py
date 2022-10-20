64b899ef2ec38af0a7d6218d9c80865bcc4f0d72
ChinmaiHK <96460827+ChinmaiHK@users.noreply.github.com>
2022-10-20 10:30:59 +0530

    Initial commit

---
+++ calc.py
@@ -0,0 +1,14 @@
+from calendar import c
+
+
+a=10
+b=50
+c=60
+r=a+b+c
+m=a*b*c
+d=b-a
+d1=c-b
+print('The sum of the three numbers is',r)
+print('The product of the three numbers is',m)
+print('The difference of the first two numbers is',d)
+print('The difference of the next two numbers is',d1)
